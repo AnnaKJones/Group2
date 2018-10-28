5456595c1143099f6caf6244ea6878e7b9113bd4
akeam <akeam.williams@yahoo.com>
2018-10-27 22:56:31 -0500

    add object model

---
+++ A02/object_model.cmd
@@ -0,0 +1,28 @@
+reset
+
+
+
+!create d1: Date
+
+!d1.Date(9, 10, 2018)
+
+!insert (d1, d1) into dNowD
+
+
+
+!create c1: Student
+!set c2.name := 'Mark Stone'
+
+
+!create c2: Student
+!set c2.name := 'Sharon Stone'
+
+
+!create d1: Date
+
+!d1.Date(18, 10, 2018)
+
+!insert (d1, d3) into dNowD
+
+
+check

962066ea17a7e9ea16bb139a3611036590885739
AnnerChan <33528929+AnnerChan@users.noreply.github.com>
2018-10-28 17:09:38 -0500

    Add files via upload
    Obj models

---
+++ A02/Maxpointsobj.cmd
@@ -0,0 +1,29 @@
+reset
+
+!create d1: Date
+!d1.Date(20, 10, 2018)
+!insert (d1,d1) into dNowD
+
+
+!create s1: Student
+!set s1.name:='Sharon Stone'
+!set s1.title:='Ms.'
+!set s1.ability:='Fire Control'
+!set s1.gender:='F'
+!set s1.points:=105
+!create d2:Date
+!d2.Date(3, 12, 2010)
+!insert (d2,d1) into dNowD
+!s1.dob:=d2
+
+!create t1: Trainer
+!set t1.name:='Evelyn Heart'
+!set t1.title:='Ms.'
+!set t1.gender:='F'
+!create d3:Date
+!d3.Date(18,9,1990)
+!insert (d3,d1) into dNowD
+!t1.dob:=d3
+
+!insert (s1,t1) into Enroll
+check

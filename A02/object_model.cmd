5e8813c7a737cb3f40c95984a6b2d63420be332a
akeam <akeam.williams@yahoo.com>
2018-10-31 14:34:35 -0500

    create and update ob,ob2 and  objmodel

--- A02/object_model.cmd
+++ A02/object_model.cmd
@@ -1,37 +1,53 @@
 reset
 !create d1:Date
-!d1.date:(9,10,2018)
+!set d1.day:= 18
 !insert(d1,d1) into dNowD
 
 !create d2:Date
-!d2.date:(18,10,2018)
+!d2.day:=20
 !insert(d2,d1) into dNowD
 
 !create s1:School
 !set s1.name:='Imperial Academy'
+!create sem1: Semester
+!create pri1:Principal
+!create rec1:Report
+!create str1:StaffRecord
+!create str2:StaffRecord
+!insert(pri1,str1) into StaffRecords
 
+!create t1:Trainer
+!insert(t1,str2) into StaffRecords
+
+
+!create cl1:Class
+!insert(cl1,s1) into Classes
+
+
+!create a1:AdmitApp
+!create pe1:Period
+
+!create p1:Parent
+!set p1.name:"Kim Black"
 !create c1:Student
 !set c1.name:='Mark Stone'
-!insert(c1,s1) into Registration
+!set c1.numberofability:=2
+!insert(p1,c1) into Parents
+!insert(c1,a1) into Application
+!create sr1:StudRecord
 
-!create c2:Student
-!set c2.name:='Sharon Stone'
-!insert(c2,s1) into Registration
+!create r1:Registration between(c1,s1)
+!create e1:Enroll between(c1,t1)
+!insert(a1,r1) into RegistrationApps
+!insert(a1,pe1) into AdmissionPeriod
+!insert(sr1,c1) into StudentRecord
+!insert(sr1,t1) into StudentRecords
+!insert(r1,sem1) into SemesterRegister
+!insert(s1,sem1) into Semesters
+!insert(e1,cl1)  into ClassOffered
+!insert(pri1,s1) into SchoolPrincipal
+!insert(pri1,rec1) into SchoolReport
+!insert(rec1,pe1) into ReportPeriod
 
-!create c3:Student
-!set c3.name:='Stone Brown'
-!insert(c3,s1) into Registration
-
-!create c4:Student
-!set c4.name:='Sharon Black'
-!insert(c4,s1) into Registration
-
-create c5:Student
-!set c5.name:='John White'
-!insert(c5,s1) into Registration
-
-!create c6:Student
-!set c6.name:='Jake Light'
-!insert(c6,s1) into Registration
 
 check

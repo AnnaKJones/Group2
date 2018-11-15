254241454839ce1b13e93b372748c71b894d5b5f
akeam <akeam.williams@yahoo.com>
2018-11-14 19:07:31 -0500

    assign-4

---
+++ T08/5a.cmd
@@ -0,0 +1,31 @@
+reset
+
+!create d1: Date
+!d1.Date(20, 10, 2018)
+
+!insert (d1,d1) into dNowD
+
+!create d2: Date
+!d2.Date(24, 10, 2018)
+!insert (d2,d1) into dNowD
+
+!create rl: RL
+
+!lp1 := rl.addLoyaltyProgram('Silver and Gold', OrderedSet{'Silver', 'Gold'})
+
+
+!create c: Customer
+
+!c.name:='Javon Nelson Price'
+
+
+!d2.Date(3, 12, 2010)
+
+!insert (d2,d1) into dNowD
+
+!c.dob:=d2
+
+
+!insert (c,lp1) into Card
+
+check
